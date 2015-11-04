20f24f49f9068a0f15855beb3959300fcbbba203
Tyler Long <tyler4long@gmail.com>
2015-11-04 17:55:27 +0800

    Do not highlight inline code

--- markdown-core.js
+++ markdown-core.js
@@ -150,7 +150,7 @@ mdc.init = function(markdown, debug) {
     console.log(result);
   }
   $('article.markdown-body').html(result);
-  $('code').each(function(i, block) {
+  $('pre > code').each(function(i, block) {
     hljs.highlightBlock(block);
   });
   if($('.mermaid').length > 0) {
