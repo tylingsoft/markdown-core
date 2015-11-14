d1d808b957e7df9c965f620fa0d3db58012bb539
Tyler Long <tyler4long@gmail.com>
2015-11-14 21:51:32 +0800

    Make icons working

--- markdown-core-new.js
+++ markdown-core-new.js
@@ -18,6 +18,20 @@ mdc = mdc.use(markdownitContainer, 'info');
 mdc = mdc.use(markdownitContainer, 'warning');
 mdc = mdc.use(markdownitContainer, 'danger');
 
+mdc = mdc.use(require('markdown-it-icon'));
+var emojione =require('emojione');
+emojione.cacheBustParam = ''; // change this to invalidate emojione icons cache
+emojione.imagePathPNG = 'https://cdn.jsdelivr.net/emojione/assets/png/';
+mdc.renderer.rules.emoji = function(tokens, idx) {
+  var shortname = tokens[idx].markup;
+  if(shortname.startsWith('fa-')) { // fontawesome
+    return '<i class="fa ' + shortname + '"></i>';
+  }
+  if(shortname.startsWith('ion-')) { // ionicons
+    return '<i class="' + shortname + '"></i>';
+  }
+  return emojione.shortnameToImage(':' + shortname + ':'); // emojione
+};
 
 mdc.init = function(markdown, debug) {
 	var result = mdc.render(markdown);
