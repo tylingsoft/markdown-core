f967dc2f11af75bf050849e7bd46d9da4de4646c
Tyler Long <tyler4long@gmail.com>
2015-11-14 12:23:04 +0800

    Enable four plugins: footnote, abbr, deflist, container

--- markdown-core-new.js
+++ markdown-core-new.js
@@ -8,6 +8,15 @@ mdc = mdc.use(require('markdown-it-mark'));
 mdc = mdc.use(require('markdown-it-ins'));
 mdc = mdc.use(require('markdown-it-sub'));
 mdc = mdc.use(require('markdown-it-sup'));
+mdc = mdc.use(require('markdown-it-footnote'));
+mdc = mdc.use(require('markdown-it-abbr'));
+mdc = mdc.use(require('markdown-it-deflist'));
+
+var markdownitContainer = require('markdown-it-container');
+mdc = mdc.use(markdownitContainer, 'success');
+mdc = mdc.use(markdownitContainer, 'info');
+mdc = mdc.use(markdownitContainer, 'warning');
+mdc = mdc.use(markdownitContainer, 'danger');
 
 
 mdc.init = function(markdown, debug) {
