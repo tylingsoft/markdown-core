833922570a0167ed0a2c22ccee3536e7eec6458a
Tyler Long <tyler4long@gmail.com>
2015-11-15 22:08:22 +0800

    Remove deploy script method

--- fabfile.py
+++ fabfile.py
@@ -1,32 +1,6 @@
 from fabric.api import local
 
 
-def deploy():
-    # copy project to /temp folder
-    local('rm -rf /tmp/markdown-core')
-    local('cp -r ../markdown-core /tmp/')
-
-    # remove unneeded folders / files
-    local('rm -rf /tmp/markdown-core/.bowerrc')
-    local('rm -rf /tmp/markdown-core/.git')
-    local('rm -rf /tmp/markdown-core/.gitignore')
-    local('rm -rf /tmp/markdown-core/bower.json')
-    local('rm -rf /tmp/markdown-core/fabfile.py')
-    local('rm -rf /tmp/markdown-core/fabfile.pyc')
-    local('rm -rf /tmp/markdown-core/index.js')
-    local('rm -rf /tmp/markdown-core/README.md')
-    local('rm -rf /tmp/markdown-core/sample.md')
-    local('rm -rf /tmp/markdown-core/dist')
-
-    # compress and mangle
-    local('mv /tmp/markdown-core/markdown-core.js /tmp/markdown-core/markdown-core.js.temp')
-    local('uglifyjs /tmp/markdown-core/markdown-core.js.temp -m -c --screw-ie8 -o /tmp/markdown-core/markdown-core.js')
-    local('rm -rf /tmp/markdown-core/markdown-core.js.temp')
-
-    # copy file to Markdown Mate project
-    local('cp -r /tmp/markdown-core ~/src/swift/Markdown\ Mate/Markdown\ Mate/')
-
-
 def dist():
     local('curl https://cdn.jsdelivr.net/github-markdown-css/2.1.0/github-markdown.css > dist/markdown-core.css')
     local('curl https://cdn.jsdelivr.net/highlight.js/8.9.1/styles/github.min.css >> dist/markdown-core.css')
