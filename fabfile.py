fad188468d3857f51f8a3d3d5eae50e2af1d3217
Tyler Long <tyler4long@gmail.com>
2015-11-15 23:49:33 +0800

    Move all css from cdn into dist/

--- fabfile.py
+++ fabfile.py
@@ -2,11 +2,111 @@ from fabric.api import local
 
 
 def dist():
-    local('curl https://cdn.jsdelivr.net/github-markdown-css/2.1.0/github-markdown.css > dist/markdown-core.css')
-    local('curl https://cdn.jsdelivr.net/highlight.js/8.9.1/styles/github.min.css >> dist/markdown-core.css')
-    local('curl https://cdn.jsdelivr.net/emojione/1.5.2/assets/css/emojione.min.css >> dist/markdown-core.css')
-    local('curl https://cdn.jsdelivr.net/mermaid/0.5.5/mermaid.css >> dist/markdown-core.css')
-    local('cat markdown-core.css >> dist/markdown-core.css')
+    local('rm -rf dist/css/*.css')
+    local('rm -rf dist/css/fonts/*')
+    local('rm -rf dist/fonts/*')
+    local('rm -rf dist/*.js')
+    local('curl https://cdn.jsdelivr.net/github-markdown-css/2.1.0/github-markdown.css > dist/css/markdown-core.css')
+    local('curl https://cdn.jsdelivr.net/highlight.js/8.9.1/styles/github.min.css >> dist/css/markdown-core.css')
+    local('curl https://cdn.jsdelivr.net/emojione/1.5.2/assets/css/emojione.min.css >> dist/css/markdown-core.css')
+    local('curl https://cdn.jsdelivr.net/mermaid/0.5.5/mermaid.css >> dist/css/markdown-core.css')
+    local('curl https://cdn.jsdelivr.net/fontawesome/4.4.0/css/font-awesome.min.css >> dist/css/markdown-core.css')
+    for font in """https://cdn.jsdelivr.net/fontawesome/4.4.0/fonts/FontAwesome.otf
+https://cdn.jsdelivr.net/fontawesome/4.4.0/fonts/fontawesome-webfont.eot
+https://cdn.jsdelivr.net/fontawesome/4.4.0/fonts/fontawesome-webfont.svg
+https://cdn.jsdelivr.net/fontawesome/4.4.0/fonts/fontawesome-webfont.ttf
+https://cdn.jsdelivr.net/fontawesome/4.4.0/fonts/fontawesome-webfont.woff
+https://cdn.jsdelivr.net/fontawesome/4.4.0/fonts/fontawesome-webfont.woff2""".split('\n'):
+        local('cd dist/fonts/ && wget ' + font)
+    local('curl https://cdn.jsdelivr.net/ionicons/2.0.1/css/ionicons.min.css >> dist/css/markdown-core.css')
+    for font in """https://cdn.jsdelivr.net/ionicons/2.0.1/fonts/ionicons.eot
+https://cdn.jsdelivr.net/ionicons/2.0.1/fonts/ionicons.svg
+https://cdn.jsdelivr.net/ionicons/2.0.1/fonts/ionicons.ttf
+https://cdn.jsdelivr.net/ionicons/2.0.1/fonts/ionicons.woff""".split('\n'):
+        local('cd dist/fonts/ && wget ' + font)
+    local('curl https://cdn.jsdelivr.net/katex/0.5.1/katex.min.css >> dist/css/markdown-core.css')
+    for font in """https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_AMS-Regular.eot
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_AMS-Regular.ttf
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_AMS-Regular.woff
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_AMS-Regular.woff2
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Caligraphic-Bold.eot
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Caligraphic-Bold.ttf
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Caligraphic-Bold.woff
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Caligraphic-Bold.woff2
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Caligraphic-Regular.eot
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Caligraphic-Regular.ttf
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Caligraphic-Regular.woff
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Caligraphic-Regular.woff2
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Fraktur-Bold.eot
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Fraktur-Bold.ttf
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Fraktur-Bold.woff
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Fraktur-Bold.woff2
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Fraktur-Regular.eot
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Fraktur-Regular.ttf
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Fraktur-Regular.woff
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Fraktur-Regular.woff2
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Main-Bold.eot
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Main-Bold.ttf
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Main-Bold.woff
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Main-Bold.woff2
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Main-Italic.eot
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Main-Italic.ttf
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Main-Italic.woff
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Main-Italic.woff2
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Main-Regular.eot
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Main-Regular.ttf
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Main-Regular.woff
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Main-Regular.woff2
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Math-BoldItalic.eot
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Math-BoldItalic.ttf
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Math-BoldItalic.woff
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Math-BoldItalic.woff2
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Math-Italic.eot
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Math-Italic.ttf
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Math-Italic.woff
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Math-Italic.woff2
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Math-Regular.eot
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Math-Regular.ttf
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Math-Regular.woff
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Math-Regular.woff2
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_SansSerif-Bold.eot
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_SansSerif-Bold.ttf
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_SansSerif-Bold.woff
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_SansSerif-Bold.woff2
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_SansSerif-Italic.eot
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_SansSerif-Italic.ttf
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_SansSerif-Italic.woff
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_SansSerif-Italic.woff2
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_SansSerif-Regular.eot
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_SansSerif-Regular.ttf
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_SansSerif-Regular.woff
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_SansSerif-Regular.woff2
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Script-Regular.eot
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Script-Regular.ttf
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Script-Regular.woff
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Script-Regular.woff2
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Size1-Regular.eot
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Size1-Regular.ttf
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Size1-Regular.woff
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Size1-Regular.woff2
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Size2-Regular.eot
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Size2-Regular.ttf
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Size2-Regular.woff
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Size2-Regular.woff2
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Size3-Regular.eot
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Size3-Regular.ttf
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Size3-Regular.woff
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Size3-Regular.woff2
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Size4-Regular.eot
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Size4-Regular.ttf
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Size4-Regular.woff
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Size4-Regular.woff2
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Typewriter-Regular.eot
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Typewriter-Regular.ttf
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Typewriter-Regular.woff
+https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_Typewriter-Regular.woff2""".split('\n'):
+        local('cd dist/css/fonts/ && wget ' + font)
+    local('cat markdown-core.css >> dist/css/markdown-core.css')
     local('browserify markdown-core-node.js -s mdc > dist/markdown-core.js')
     local('echo "\n" >> dist/markdown-core.js')
     local('curl https://cdn.jsdelivr.net/jquery/2.1.4/jquery.min.js >> dist/markdown-core.js')
@@ -17,7 +117,6 @@ def dist():
     local('uglifyjs dist/markdown-core.js -cmo dist/markdown-core.min.js')
 
 
-# copy code to Markdown Mate project
-def mdm():
+def mdm(): # copy dist code to Markdown Mate project
     local('cp index.html "~/src/swift/markdown-mate/Markdown Mate/markdown-core/"')
     local('cp -r dist "~/src/swift/markdown-mate/Markdown Mate/markdown-core/"')
