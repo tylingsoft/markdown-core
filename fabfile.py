edbf83c4cc2a8306057a8469021ee5cf9352caf6
Tyler Long <tyler4long@gmail.com>
2015-11-15 18:20:02 +0800

    Merge github-markdown-css and highlight.js css into dist

--- fabfile.py
+++ fabfile.py
@@ -28,7 +28,9 @@ def deploy():
 
 
 def dist():
-    local('curl https://cdn.jsdelivr.net/mermaid/0.5.5/mermaid.css > dist/markdown-core.css')
+    local('curl https://cdn.jsdelivr.net/github-markdown-css/2.1.0/github-markdown.css > dist/markdown-core.css')
+    local('curl https://cdn.jsdelivr.net/highlight.js/8.9.1/styles/github.min.css >> dist/markdown-core.css')
+    local('curl https://cdn.jsdelivr.net/mermaid/0.5.5/mermaid.css >> dist/markdown-core.css')
     local('cat markdown-core.css >> dist/markdown-core.css')
     local('browserify markdown-core-node.js -s mdc > dist/markdown-core.js')
     local('curl https://cdn.jsdelivr.net/mermaid/0.5.5/mermaid.min.js >> dist/markdown-core.js')
