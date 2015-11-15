bf439378332b0c7441b7a24e5f8a9d8f8eb1e911
Tyler Long <tyler4long@gmail.com>
2015-11-15 18:28:23 +0800

    Merge emojione css into dist

--- fabfile.py
+++ fabfile.py
@@ -30,6 +30,7 @@ def deploy():
 def dist():
     local('curl https://cdn.jsdelivr.net/github-markdown-css/2.1.0/github-markdown.css > dist/markdown-core.css')
     local('curl https://cdn.jsdelivr.net/highlight.js/8.9.1/styles/github.min.css >> dist/markdown-core.css')
+    local('curl https://cdn.jsdelivr.net/emojione/1.5.2/assets/css/emojione.min.css >> dist/markdown-core.css')
     local('curl https://cdn.jsdelivr.net/mermaid/0.5.5/mermaid.css >> dist/markdown-core.css')
     local('cat markdown-core.css >> dist/markdown-core.css')
     local('browserify markdown-core-node.js -s mdc > dist/markdown-core.js')
