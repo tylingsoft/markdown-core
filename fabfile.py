aeada1600baf20efd5bf6fc2522195b03bac308a
Tyler Long <tyler4long@gmail.com>
2016-01-07 21:37:15 +0800

    Update dist folder structure

--- fabfile.py
+++ fabfile.py
@@ -9,25 +9,23 @@ def update():
 def css():
     local('rm -rf dist/css/*.css')
     local('rm -rf dist/css/fonts/*')
-    local('rm -rf dist/fonts/*')
     local('curl https://cdn.jsdelivr.net/github-markdown-css/2.1.1/github-markdown.css > dist/css/markdown-core.css')
     local('curl https://cdn.jsdelivr.net/highlight.js/9.0.0/styles/github.min.css >> dist/css/markdown-core.css')
     local('curl https://cdn.jsdelivr.net/emojione/2.0.0/assets/css/emojione.min.css >> dist/css/markdown-core.css')
     local('curl https://cdn.jsdelivr.net/mermaid/0.5.6/mermaid.css >> dist/css/markdown-core.css')
-    local('curl https://cdn.jsdelivr.net/fontawesome/4.5.0/css/font-awesome.min.css >> dist/css/markdown-core.css')
-    for font in """https://cdn.jsdelivr.net/fontawesome/4.5.0/fonts/FontAwesome.otf
-https://cdn.jsdelivr.net/fontawesome/4.5.0/fonts/fontawesome-webfont.eot
+    local('curl https://cdn.jsdelivr.net/fontawesome/4.5.0/css/font-awesome.min.css | sed "s/..\/fonts\//fonts\//g" >> dist/css/markdown-core.css')
+    for font in """https://cdn.jsdelivr.net/fontawesome/4.5.0/fonts/fontawesome-webfont.eot
 https://cdn.jsdelivr.net/fontawesome/4.5.0/fonts/fontawesome-webfont.svg
 https://cdn.jsdelivr.net/fontawesome/4.5.0/fonts/fontawesome-webfont.ttf
 https://cdn.jsdelivr.net/fontawesome/4.5.0/fonts/fontawesome-webfont.woff
 https://cdn.jsdelivr.net/fontawesome/4.5.0/fonts/fontawesome-webfont.woff2""".split('\n'):
-        local('cd dist/fonts/ && wget ' + font)
-    local('curl https://cdn.jsdelivr.net/ionicons/2.0.1/css/ionicons.min.css >> dist/css/markdown-core.css')
+        local('cd dist/css/fonts/ && wget ' + font)
+    local('curl https://cdn.jsdelivr.net/ionicons/2.0.1/css/ionicons.min.css | sed "s/..\/fonts\//fonts\//g" >> dist/css/markdown-core.css')
     for font in """https://cdn.jsdelivr.net/ionicons/2.0.1/fonts/ionicons.eot
 https://cdn.jsdelivr.net/ionicons/2.0.1/fonts/ionicons.svg
 https://cdn.jsdelivr.net/ionicons/2.0.1/fonts/ionicons.ttf
 https://cdn.jsdelivr.net/ionicons/2.0.1/fonts/ionicons.woff""".split('\n'):
-        local('cd dist/fonts/ && wget ' + font)
+        local('cd dist/css/fonts/ && wget ' + font)
     local('curl https://cdn.jsdelivr.net/katex/0.5.1/katex.min.css >> dist/css/markdown-core.css')
     for font in """https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_AMS-Regular.eot
 https://cdn.jsdelivr.net/katex/0.5.1/fonts/KaTeX_AMS-Regular.ttf
