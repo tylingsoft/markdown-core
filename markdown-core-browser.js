1f9d4200b2236fdd690c952dfa4d49e9c73f2490
Tyler Long <tyler4long@gmail.com>
2016-12-30 15:58:27 +0800

    Load js-cookie with webpack

--- markdown-core-browser.js
+++ markdown-core-browser.js
@@ -1,6 +1,7 @@
 const mdc = require('./markdown-core-node')
 require('chart.js')
 const $ = require('jquery')
+const Cookies = require('js-cookie')
 
 // convert an element to png image, aka screenshot an element
 mdc.elementToPng = function($element) {
