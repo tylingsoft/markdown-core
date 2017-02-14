a46cf831b56b78528972236a7ef912581a4e22a8
Tyler Long <tyler4long@gmail.com>
2017-02-14 21:13:40 +0800

    Remove commented code

--- webpack.config.js
+++ webpack.config.js
@@ -34,31 +34,6 @@ rules.push({
   }
 })
 
-// const loaders = [
-//   {
-//     test: /\.js$/,
-//     loader: 'babel-loader',
-//     exclude: /node_modules/,
-//     query: {
-//       presets: [
-//         ['env', {
-//           'targets': {
-//             'browsers': ['last 2 versions']
-//           }
-//         }]
-//       ]
-//     }
-//   },
-//   {
-//     test: /\.css$/,
-//     loader: 'style-loader!css-loader'
-//   },
-//   {
-//     test: /\.(ttf|eot|svg|woff2?)(\?v=.+?)?$/,
-//     loader: 'url-loader?limit=1000000'
-//   }
-// ]
-
 const config = {
   target: 'web',
   entry: {
