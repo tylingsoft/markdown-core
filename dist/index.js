429f285c25152a018ebc8a049392d14dfb372841
Tyler Long <tyler4long@gmail.com>
2017-04-07 17:53:18 +0800

    No adjust to anchor scrolling

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,6 @@ import $ from 'jquery'
 const request = new window.XMLHttpRequest()
 request.onload = function () {
   mdc.init(this.response, false)
-  window.addEventListener('hashchange', () => { window.scrollBy(0, -8) }) // a little gap to top
   if (window.location.hash.length > 0) {
     $('html,body').scrollTop($(window.location.hash).offset().top - 8) // scroll to hash element
   }
