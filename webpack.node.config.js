df4fc43a14bb23382197bfd3be78028d21d9dc81
Tyler Long <tyler4long@gmail.com>
2017-04-07 10:08:31 +0800

    Separate webpack config file for node and web

--- webpack.node.config.js
+++ webpack.node.config.js
@@ -2,18 +2,14 @@ import path from 'path'
 import nodeExternals from 'webpack-node-externals'
 import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
-const commonRules = [
+const nodeRules = [
   {
     test: /\.css$/,
     use: ExtractTextPlugin.extract({
       fallback: 'style-loader',
       use: 'css-loader'
     })
-  }
-]
-
-const nodeRules = [
-  ...commonRules,
+  },
   {
     test: /\.js$/,
     use: {
@@ -31,7 +27,7 @@ const nodeRules = [
   }
 ]
 
-const nodeConfig = {
+const config = {
   target: 'node',
   externals: [nodeExternals()],
   entry: {
@@ -48,38 +44,4 @@ const nodeConfig = {
   ]
 }
 
-const webRules = [
-  ...commonRules,
-  {
-    test: /\.js$/,
-    use: {
-      loader: 'babel-loader',
-      options: {
-        presets: [
-          ['env', {
-            'targets': {
-              'browsers': ['last 2 versions']
-            }
-          }]
-        ]
-      }
-    }
-  }
-]
-
-const webConfig = {
-  target: 'web',
-  entry: {
-    'web': './dist/index.js'
-  },
-  output: {
-    path: path.join(__dirname, './dist/'),
-    filename: '[name].bundle.js'
-  },
-  module: { rules: webRules },
-  plugins: [
-    new ExtractTextPlugin('index.bundle.css')
-  ]
-}
-
-export default [nodeConfig, webConfig]
+export default [config]
