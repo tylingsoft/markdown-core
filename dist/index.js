4da7aee347b65dd5b1d0c5be88c03dbd56502b52
Tyler Long <tyler4long@gmail.com>
2017-04-07 17:46:27 +0800

    Get rid of $.get()

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,13 @@
 import mdc from '../src/index-browser'
 import $ from 'jquery'
 
-$.get('sample.md', (data) => {
-  mdc.init(data, false)
+const request = new window.XMLHttpRequest()
+request.onload = function () {
+  mdc.init(this.response, false)
   window.addEventListener('hashchange', () => { window.scrollBy(0, -8) }) // a little gap to top
   if (window.location.hash.length > 0) {
     $('html,body').scrollTop($(window.location.hash).offset().top - 8) // scroll to hash element
   }
-})
+}
+request.open('GET', 'sample.md')
+request.send()
