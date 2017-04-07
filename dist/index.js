2801ccc23017230b31f03d7653efd36683ff1b41
Tyler Long <tyler4long@gmail.com>
2017-04-07 17:35:55 +0800

    Get rid of $()

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,10 @@
 import mdc from '../src/index-browser'
 import $ from 'jquery'
 
-$(() => {
-  $.get('sample.md', (data) => {
-    mdc.init(data, false)
-    window.addEventListener('hashchange', () => { window.scrollBy(0, -8) }) // a little gap to top
-    if (window.location.hash.length > 0) {
-      $('html,body').scrollTop($(window.location.hash).offset().top - 8) // scroll to hash element
-    }
-  })
+$.get('sample.md', (data) => {
+  mdc.init(data, false)
+  window.addEventListener('hashchange', () => { window.scrollBy(0, -8) }) // a little gap to top
+  if (window.location.hash.length > 0) {
+    $('html,body').scrollTop($(window.location.hash).offset().top - 8) // scroll to hash element
+  }
 })
