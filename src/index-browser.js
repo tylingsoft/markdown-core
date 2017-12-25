a80d0e27212f94ef545ff9630492d5f358653550
Tyler Long <tyler4long@gmail.com>
2017-12-25 15:24:48 +0800

    Customize mermaid

--- src/index-browser.js
+++ src/index-browser.js
@@ -6,20 +6,26 @@ import Cookies from 'js-cookie'
 
 import mermaid from 'mermaid'
 
-// import 'mermaid/dist/mermaid.css'
 import 'markdown-it-latex/dist/index.css'
 import 'markdown-it-icons/dist/index.css'
 import 'markdown-it-highlight/dist/index.css'
 import './index.css'
 
 window.mermaid = mermaid
+window.mermaid.initialize({
+  theme: 'forest',
+  gantt: { axisFormatter: [
+    ['%Y-%m-%d', (d) => {
+      return d.getDay() === 1
+    }]
+  ] }
+})
 
 mdc.loadPreferences = () => {
   let ganttAxisFormat = Cookies.get('gantt-axis-format')
   if (ganttAxisFormat === undefined) {
     ganttAxisFormat = '%Y-%m-%d'
   }
-  // window.mermaid.gantt.axisFormat(ganttAxisFormat)
   return { 'gantt-axis-format': ganttAxisFormat }
 }
 mdc.loadPreferences()
